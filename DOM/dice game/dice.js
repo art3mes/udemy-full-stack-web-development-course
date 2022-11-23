99cf0966a13839469c42287185a646c1b255f594
art3mes <93867313+art3mes@users.noreply.github.com>
2022-11-23 17:07:21 +0530

    DICE GAME done?

--- DOM/dice game/dice.js
+++ DOM/dice game/dice.js
@@ -1,8 +1,22 @@
-var diceNum=Math.floor(Math.random()*6)+1;
 
 
 function changePicture()
 {
+    var diceNum=Math.floor(Math.random()*6)+1;
+    var diceNum2=Math.floor(Math.random()*6)+1;
     document.querySelector(".img1").setAttribute("src","images/dice"+diceNum+".png");
+    document.querySelector(".img2").setAttribute("src","images/dice"+diceNum2+".png");
+
+    if(diceNum<diceNum2)
+    {
+        document.querySelector(".container h1").innerHTML="Player 2 WINS 🚩";
+    }
+    else if(diceNum===diceNum2)
+    {
+        document.querySelector(".container h1").innerHTML="Its a TIE! ";
+    }
+    else{
+        document.querySelector(".container h1").innerHTML="🚩 PLayer 1 WINS!"; 
+    }
 }
 
