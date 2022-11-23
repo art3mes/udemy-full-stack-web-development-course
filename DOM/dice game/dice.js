86049af546bd07eaed3861cf8acc7ca8e784c91b
art3mes <93867313+art3mes@users.noreply.github.com>
2022-11-23 16:59:32 +0530

    LMOA

--- DOM/dice game/dice.js
+++ DOM/dice game/dice.js
@@ -1,1 +1,8 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
+var diceNum=Math.floor(Math.random()*6)+1;
+
+
+function changePicture()
+{
+    document.querySelector(".img1").setAttribute("src","images/dice"+diceNum+".png");
+}
+
