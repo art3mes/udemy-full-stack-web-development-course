32dee8ef455b3d0406373f37f059c98d8b907809
art3mes <93867313+art3mes@users.noreply.github.com>
2022-11-24 20:47:28 +0530

    dice game done

--- DOM/dice game/dice.js
+++ DOM/dice game/dice.js
@@ -1,11 +1,7 @@
-
-
-function changePicture()
-{
     var diceNum=Math.floor(Math.random()*6)+1;
     var diceNum2=Math.floor(Math.random()*6)+1;
-    document.querySelector(".img1").setAttribute("src","images/dice"+diceNum+".png");
-    document.querySelector(".img2").setAttribute("src","images/dice"+diceNum2+".png");
+    document.querySelector(".img1").setAttribute("src","images/dice"+diceNum+".png");  //document.querySelectorAll("img")[0]
+    document.querySelector(".img2").setAttribute("src","images/dice"+diceNum2+".png"); //document.querySelectorAll("img")[1]
 
     if(diceNum<diceNum2)
     {
@@ -18,5 +14,3 @@ function changePicture()
     else{
         document.querySelector(".container h1").innerHTML="🚩 PLayer 1 WINS!"; 
     }
-}
-
