37be0afba2f6a111de241c6d0c5037afc44a8c63
art3mes <93867313+art3mes@users.noreply.github.com>
2022-11-26 22:45:22 +0530

    L162

--- DOM/drum kit/drum.js
+++ DOM/drum kit/drum.js
@@ -3,4 +3,10 @@ function handleClick()
     alert("harder~");
 }
 
-document.querySelector("button").addEventListener("click",handleClick);
+for(var a=0;a<document.querySelectorAll(".drum").length;a++)      // be specific in the class name. adding "button" will just loop through
+                                                                  //all the buttons in the document;   
+{
+    document.querySelectorAll("button")[a].addEventListener("click",handleClick);
+}
+
+//for(var a=0;a<7;a++) {}            itll also work ;)
