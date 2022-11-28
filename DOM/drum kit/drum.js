d79ecce6f3b2c4fd1913cf6d5ae5ed12481e44d1
art3mes <93867313+art3mes@users.noreply.github.com>
2022-11-28 20:00:29 +0530

    to be continued..

--- DOM/drum kit/drum.js
+++ DOM/drum kit/drum.js
@@ -1,22 +1,17 @@
 function handleClick()
 {
-    var audio=new Audio("sounds/snare.mp3");
-    audio.play();
+    // var audio=new Audio("sounds/snare.mp3");
+    // audio.play();
 
 }
 
 for(var a=0;a<document.querySelectorAll(".drum").length;a++)      // be specific in the class name. adding "button" will just loop through
                                                                   //all the buttons in the document;   
+                                                                  //for(var a=0;a<7;a++) {}            itll also work ;)
 {
     document.querySelectorAll("button")[a].addEventListener("click",handleClick);
     
 }
-//for(var a=0;a<7;a++) {}            itll also work ;)
 
-function sound()
-{
-
-   
-}
 
 
