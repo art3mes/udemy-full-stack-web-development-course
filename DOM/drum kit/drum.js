2ff916e4f4da2cd8f305259c01f97e320cb33358
art3mes <93867313+art3mes@users.noreply.github.com>
2022-11-28 19:34:06 +0530

    sounds

--- DOM/drum kit/drum.js
+++ DOM/drum kit/drum.js
@@ -8,5 +8,11 @@ for(var a=0;a<document.querySelectorAll(".drum").length;a++)      // be specific
 {
     document.querySelectorAll("button")[a].addEventListener("click",handleClick);
 }
-
 //for(var a=0;a<7;a++) {}            itll also work ;)
+
+function sound()
+{
+    audio.play();
+}
+var audio=new Audio("sounds/snare.mp3");
+
