b46fe9548bd5eb67cbae34fa1a43493e290682a0
art3mes <93867313+art3mes@users.noreply.github.com>
2022-11-30 21:02:32 +0530

    each button sounds

--- DOM/drum kit/drum.js
+++ DOM/drum kit/drum.js
@@ -1,16 +1,55 @@
+
+// be specific in the class name. adding "button" will just loop through all the buttons in the document;   
+//for(var a=0;a<7;a++) {}            itll also work ;)
+
+for(var a=0;a<document.querySelectorAll(".drum").length;a++)
+{
+    document.querySelectorAll("button")[a].addEventListener("click",handleClick);   
+}
 function handleClick()
 {
-    this.style.color="white";     //change color of each text to white
+    //this.style.color="white";     change color of each text to white
+    var buttonValue=this.innerHTML;
 
-}
-
-for(var a=0;a<document.querySelectorAll(".drum").length;a++)      // be specific in the class name. adding "button" will just loop through
-                                                                  //all the buttons in the document;   
-                                                                  //for(var a=0;a<7;a++) {}            itll also work ;)
-{
-    document.querySelectorAll("button")[a].addEventListener("click",handleClick);
+    switch (buttonValue) {
+        case "w":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick=new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;    
     
+        default:
+            console.log(buttonValue);
+            break;
+    }
 }
+
+
+
+
 
 
 
