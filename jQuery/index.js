7aad87b5e08ab7ff3f3b7c38c4c6ee5f7be2bcc6
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-17 21:16:23 +0530

    176

---
+++ jQuery/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){                 /*runs when the jQuery CDN is ready. a type of failsafe. so you can place the jQuery CDN anywhere*/
+    $("h1").css("color","red");               /* to avoid using this. transfer the CDN and script tags to the last of the body tag*/
+});
