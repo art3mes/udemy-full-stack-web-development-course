c765ea1b3ced32d5b421cbad499b32077541b03a
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-18 18:45:40 +0530

    179

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){                 /*runs when the jQuery CDN is ready. a type of failsafe. so you can place the jQuery CDN anywhere*/
-    $("h1").css("color","red");               /* to avoid using this. transfer the CDN and script tags to the last of the body tag*/
-});
+                                              /* to avoid using this. transfer the CDN and script tags to the last of the body tag*/
+
+$("buttons");                     /*will select all buttons */
+$("h1");                          /*will select h1*/
+
+$("css-selector").addClass("class1 class2 class3");         /*will add a class*/
+$("css-selector").removeClass("class1 class2");        /*to remove class*/
+$("css-selector").hasClass("class1 class2 class3");          /*returns boolean/
+console.log($("h1").css("color"));                            /*will get the color*/
+
+$("h1").css("color","blue");                               /*will change the color*/
+
+
+
+
+
+
+
+});                                                   
