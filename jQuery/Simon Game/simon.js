0671257e0728e64a169e8865457bdb23ea6a90fb
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-22 13:08:25 +0530

    202 step 7

--- jQuery/Simon Game/simon.js
+++ jQuery/Simon Game/simon.js
@@ -1,22 +1,17 @@
 var buttonColors=["red","blue","green","yellow"];
 var gamePattern= [];
 var userClickedPattern=[];
+var gameStarter=0;
+var level=0;
 
 function nextSequence(){
+    level++;
     var randomChosenColor=buttonColors[Math.floor(Math.random()*4)];
     gamePattern.push(randomChosenColor);
     $("#"+randomChosenColor).fadeOut(180).fadeIn(180);
     playSound(randomChosenColor);
     
 }
-$(".btn").click(function(event){
-    var userChosenColor=event.target.id;
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
-});
-
 function playSound(name){
     var audio = new Audio('sounds/'+name+'.mp3');
     audio.play();
@@ -27,3 +22,18 @@ function animatePress(currentColor){
         $('.'+currentColor).removeClass('pressed');
     },150);
 }
+$(".btn").click(function(event){
+    var userChosenColor=event.target.id;
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
+$(document).keypress(function(event){
+    if((event.key=="a"||event.key=="A") && gameStarter=="0"){
+        $("h1").text("Level "+level);
+        nextSequence();
+        gameStarter=1;
+    }
+}); 
+
