88db805f7abf4c73a34bd0d661ca7f86ab1fb565
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-26 16:23:42 +0530

    npm packages (practice)

---
+++ nodeJS/npm/index.js
@@ -0,0 +1,9 @@
+const superheroes=require("superheroes");
+
+var heroName=superheroes.random();
+console.log("SuperHero: "+heroName);
+
+const supervillains=require("supervillains");
+
+var villainName=supervillains.random();
+console.log("SuperVillain: "+villainName);
