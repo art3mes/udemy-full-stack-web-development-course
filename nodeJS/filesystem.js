5021b729e8d818b82399ddb958d15417117e3705
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-26 16:22:57 +0530

    ok

--- nodeJS/filesystem.js
+++ nodeJS/filesystem.js
@@ -1,3 +1,5 @@
 const fs=require("fs");
 
 fs.copyFileSync("dontCopyThisFile.txt","fileCopied.txt");
+
+//run it using node "node filesystem.js"
--- nodeJS/index.js
+++ nodeJS/index.js
@@ -1,3 +1,10 @@
 console.log("Helo Boy!");
 console.log("node and press enter to enter into the REPL(Read Evaluation Print Loops) mode");
 console.log(" ctrl+c twice to exit that node or .exit");
+
+// Node.js is a cross-platform, 
+// open-source server environment that can run on Windows, Linux, Unix, macOS, and more. 
+
+// Node.js is a back-end JavaScript runtime environment, 
+// runs on the V8 JavaScript Engine, 
+// and executes JavaScript code outside a web browser.
