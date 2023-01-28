f3221c9a68741e439e0e75cfd57b827dc198063d
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-28 12:18:43 +0530

    sending response when homepage is called upon

--- nodeJS/npm/my-express-server/server.js
+++ nodeJS/npm/my-express-server/server.js
@@ -6,6 +6,13 @@
 const express=require("express");        //to get the express module
 const app = express();                 // binding "app" variable to express() function
 
+app.get("/", function(request,response){        //will decide what it should send when the the browser requests the server that is started below
+                                                // "/" means the root directory,ie, homepage of the server.
+    //console.log(request);                      will send all the information to the TERMINAL regarding the request made.
+    response.send("lmao");                            //to send the response
+});                             
+                                        
+
 app.listen(3000,function(){
     console.log("server started on Port 3000")
 });                       //listening to a port numbered 3000 with a callback function
