2243d1666fe899a66596c00c9da9a18c5791ec58
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-28 12:32:14 +0530

    Routes

--- nodeJS/npm/my-express-server/server.js
+++ nodeJS/npm/my-express-server/server.js
@@ -7,11 +7,18 @@ const express=require("express");        //to get the express module
 const app = express();                 // binding "app" variable to express() function
 
 app.get("/", function(request,response){        //will decide what it should send when the the browser requests the server that is started below
+                                                // first parameter is the route, second is the callback function
                                                 // "/" means the root directory,ie, homepage of the server.
     //console.log(request);                      will send all the information to the TERMINAL regarding the request made.
     response.send("lmao");                            //to send the response
 });                             
-                                        
+                   
+app.get("/contact", function(request,response){         //another route
+    response.send("<h1>dont contact me. what do you want :(</h1>")
+});
+app.get("/about", function(req,res){                    //another route
+    res.send("NO.");
+});
 
 app.listen(3000,function(){
     console.log("server started on Port 3000")
