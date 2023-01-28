2ec0b2a4e467ec26c33cb2a6ebd24c07f0979f3b
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-28 11:55:58 +0530

    227

--- nodeJS/npm/my-express-server/server.js
+++ nodeJS/npm/my-express-server/server.js
@@ -1,3 +1,8 @@
+// Express.js, or simply Express, is a back end web application framework for building RESTful APIs with Node.js, 
+// released as free and open-source software under the MIT License. 
+// It is designed for building web applications and APIs. 
+// It has been called the de facto standard server framework for Node.js.
+
 const express=require("express");        //to get the express module
 const app = express();                 // binding "app" variable to express() function
 
