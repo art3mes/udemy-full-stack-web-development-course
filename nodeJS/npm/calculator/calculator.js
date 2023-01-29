dc7d28b7df9ac4177c2b77ee5f817096dc69363a
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-29 19:20:38 +0530

    sending files to server

--- nodeJS/npm/calculator/calculator.js
+++ nodeJS/npm/calculator/calculator.js
@@ -2,7 +2,8 @@ const express=require("express");
 const app=express();
 
 app.get("/", function(request,response){
-    response.send("lmao");
+    //response.send("lmao");
+    response.sendFile(__dirname+"/index.html");             //__dirname is a constant which gives full path of the current working directory. try printing it
 });
 
 app.listen(3000, function(){
