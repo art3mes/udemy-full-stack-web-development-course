3501e89342d0dfa5c929dee7205d20677f0b1480
art3mes <93867313+art3mes@users.noreply.github.com>
2023-02-22 22:34:12 +0530

    parsing JSON

--- API/weather-project/app.js
+++ API/weather-project/app.js
@@ -1,3 +1,4 @@
+const { ok } = require("assert");
 const express=require ("express");
 const https=require ("https");             // native node module. used to get get requests
 
@@ -7,9 +8,22 @@ const app=express();
 
 app.get("/",function(req,res){
 
-    const url="https://api.openweathermap.org/data/2.5/weather?q=lucknow&appid=719d71f2bbb18d4b4268ca4c221dd7d9&units=metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q=rome&appid=719d71f2bbb18d4b4268ca4c221dd7d9&units=metric";
     https.get(url,function(response){           //it gets a response from the send url
-        console.log(response);
+        console.log(response.statusCode);               //https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+                                                        // 200     ok
+                                                        // 404     resourse not found
+                                                        // 401     unauthorized
+        response.on("data",function(data){                  // on function to interact with the recieved data
+           // console.log(data);      // will give the output in the hexadecimal format
+           const weatherDATA= JSON.parse(data);       //will convert the data into a json object
+            //weatherDATA being the JSON object              
+            //JSON.stringify(data);   will convert the json objject to strings (tightly packed)
+            console.log(weatherDATA);   //will print the whole json object
+            console.log("temperature: ",weatherDATA.main.temp);
+            console.log("Description: ",weatherDATA.weather[0].description);
+
+        });
     });
     res.send("server is running");
 });
