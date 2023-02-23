5ca6c02ef090e9cbd20c124299044e0e0e6faab2
art3mes <93867313+art3mes@users.noreply.github.com>
2023-02-23 23:15:41 +0530

    error

--- API/newsletter-signup/app.js
+++ API/newsletter-signup/app.js
@@ -1,12 +1,14 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const request=require("require");         //deprecated package
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/",function(req,res){
-    res.send("lmao");
+//    res.sendFile(__filename+ "/signup.html");
+    res.send("LMAO");
 });
 
 app.listen(3000,function(){
