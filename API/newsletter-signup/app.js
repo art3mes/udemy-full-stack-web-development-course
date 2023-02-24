f1b01d52e6f585dbba4c07508bfe5714b69c3045
art3mes <93867313+art3mes@users.noreply.github.com>
2023-02-25 00:15:58 +0530

    server connected to the mailchimp api. working perfectly

--- API/newsletter-signup/app.js
+++ API/newsletter-signup/app.js
@@ -1,5 +1,6 @@
 const express=require ("express");
 const bodyParser=require("body-parser");
+const https=require("https");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,13 +13,45 @@ app.get("/", function(req,res){
 });
 
 app.post("/", function(req,res){
-    var firstName=req.body.firstName;
-    var lastName=req.body.lastName;
-    var email=req.body.email;
+    const firstName=req.body.firstName;
+    const lastName=req.body.lastName;
+    const email=req.body.email;
 
-    console.log(firstName,lastName,email);
+    const data={
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields: {
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    };
+    const jsonDATA=JSON.stringify(data);
+
+    const url="https://us18.api.mailchimp.com/3.0/lists/d1c66f90cb";
+    const options={
+        method:"POST",
+        auth:"reiner1:051b45dcbad1b93e5ac648fe8c797a13-us18"
+    };
+    const request=https.request(url,options, function(response){          //save value in a constant. then send it to mailchimp server
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+    request.write(jsonDATA);
+    request.end();
+    // console.log(firstName,lastName,email);
 });
 
 app.listen(3000, function(){
     console.log("Server is up and running!");
 });
+
+// API
+// 051b45dcbad1b93e5ac648fe8c797a13-us18
+
+// list id
+// d1c66f90cb
