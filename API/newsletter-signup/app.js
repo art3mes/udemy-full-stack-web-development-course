3b0ea5f5601a9c90120a06f31985fecbdfffda52
art3mes <93867313+art3mes@users.noreply.github.com>
2023-02-24 22:12:09 +0530

    big bug fixed

--- API/newsletter-signup/app.js
+++ API/newsletter-signup/app.js
@@ -4,11 +4,20 @@ const bodyParser=require("body-parser");
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static("public"));         //THIS THING> BRUH. USE THIS< MAKE A FOLDER NAMED PUBLIC AND FOLLOW TO ENABLE CSS
+                                            //website will not work normally after this. use server
 
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/signup.html");
 });
 
+app.post("/", function(req,res){
+    var firstName=req.body.firstName;
+    var lastName=req.body.lastName;
+    var email=req.body.email;
+
+    console.log(firstName,lastName,email);
+});
 
 app.listen(3000, function(){
     console.log("Server is up and running!");
