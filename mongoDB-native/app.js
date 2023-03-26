4142f1775815e2af5b43c092901bcd91555ab312
art3mes <93867313+art3mes@users.noreply.github.com>
2023-03-26 12:33:50 +0530

    mongoDB community connected to node app

--- mongoDB-native/app.js
+++ mongoDB-native/app.js
@@ -1,52 +1,56 @@
-// const MongoClient =require('mongodb').MongoClient;
-// const assert =require('assert');                        //used to do data checking. or input validation
-
-
-// //connection url
-// const url ='mongodb://localhost:27017';
-// const options = { family: 4 };
-
-// //database name
-// const dbName= 'fruitsDB';
-
-// //create a new mongoClient
-// const client = new MongoClient(url, options);
-
-// //use connect method to connect to the server
-// client.connect(function(err){
-//     assert.equal(null, err);
-//     console.log("COnnected to the server.");
-
-//     const db=client.db(dbName);
-//     client.close();
-//     }
-// )
-// const MongoClient = require('mongodb').MongoClient;
-
-// const url = 'mongodb://localhost:27017';
-// const dbName = 'fruitsDB';
-
-// MongoClient.connect(url, function(err, client) {
-//   if (err) throw err;
-
-//   const db = client.db(dbName);
-//   console.log("Connected successfully to server");
-
-//   // Do something with the database connection here...
-
-//   client.close();
-// });
-
-
 // CdBGzIXmqjL9u2Zq
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://127.0.0.1:27017';
+const options = { family: 4 };                        //changin the IP formatting to IPv4 instead of IPv6 . read npm docs
+const client = new MongoClient(url, options);
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://madscientistnumber1:LMAODEAD@cluster0.wryl2tc.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("connected");
-  // perform actions on the collection object
-  client.close();
-});
+// Database Name
+const dbName = 'fruitsDB';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('fruits');
+
+  const insertResult = await collection.insertMany([                //inserting 3 elements
+    { name : "Pineapple", rating: 5,review: "not bad"}, 
+    {name : "Banana",rating: 10,review: "banana"}, 
+    {name : "orange",rating: 4,review: "sour"}
+  ]);
+  // console.log('Inserted documents =>', insertResult);
+  console.log("3 elements inserted");
+  const findResult = await collection.find({}).toArray();          //printing the inserted elements
+console.log('Found documents =>', findResult);
+
+  return 'done. exit.';
+}
+// const insertDocuments = function(db, callback) {                    //OLD
+//   // Get the documents collection
+//   console.log("insert");
+//   const collection = db.collection('fruits');
+//   // Insert some documents
+//   collection.insertMany([
+//     { name : "Pineapple", rating: 5,review: "not bad"}, 
+//     {name : "Banana",rating: 10,review: "banana"}, 
+//     {name : "orange",rating: 4,review: "sour"}
+//   ], function(err, result) {
+//     assert.equal(err, null);                     //validating input
+//     assert.equal(3, result.result.n);               //checking if the inputs are three or not
+//     assert.equal(3, result.ops.length);
+//     console.log("Inserted 3 documents into the collection");
+//     callback(result);
+//   });
+// }
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+  
