d95618a9d7b26a23eba8ac9affb4f10475ba631e
art3mes <93867313+art3mes@users.noreply.github.com>
2023-05-07 18:07:52 +0530

    adding mongoose connection

--- wikiAPI/app.js
+++ wikiAPI/app.js
@@ -9,6 +9,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 
+mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
+const articleSchema={
+    title:String,
+    content:String
+};
+const Article=mongoose.model('Article',articleSchema);
+
 app.listen(3000, function(){
     console.log("Server is running on the port 3000");
-})
+});
