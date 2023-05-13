b3f47dac08480249b4e2e24417cca05df4d3ec11
art3mes <93867313+art3mes@users.noreply.github.com>
2023-05-13 12:30:15 +0530

    case sensitive url

--- wikiAPI/app.js
+++ wikiAPI/app.js
@@ -2,6 +2,7 @@ const express=require ("express");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const bodyParser =require("body-parser");
+const _=require("lodash");
 
 const app=express();
 app.set('view engine','ejs');
