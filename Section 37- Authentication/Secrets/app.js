bd9021aa8b2d160d944996e9be25cc869db54852
art3mes <93867313+art3mes@users.noreply.github.com>
2023-05-22 15:14:27 +0530

    login page implemented

--- Section 37- Authentication/Secrets/app.js
+++ Section 37- Authentication/Secrets/app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 const userSchema={
-    username:String,
+    email:String,
     password: String
 };
 const User=mongoose.model('User',userSchema);
@@ -24,7 +24,7 @@ app.get("/register",function(req,res){
 });
 app.post("/register", function(req,res){
     const newUser=new User({
-        username:req.body.username,
+        email:req.body.username,
         password:req.body.password
     });
     newUser.save()
@@ -36,7 +36,24 @@ app.post("/register", function(req,res){
         res.send(err);
       });
 });
+app.post("/login",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
 
+    User.findOne({email:username})
+    .then((foundUser)=>{
+
+        if(foundUser.password===password){
+            res.render("secrets");
+        } 
+    })
+    .catch((err)=>{
+        //console.log("An error occurred while finding the article:", err);
+        res.status(500).send("Requested User does not exist.");
+        console.log(err);
+    });
+
+});
 app.listen(3000,function(req,res){
     console.log("Server is running at the Port 3000");
 });
