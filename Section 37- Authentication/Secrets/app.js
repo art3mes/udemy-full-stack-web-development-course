6c763473aa9347c33b831d8f2b8282a01d9a4dba
art3mes <93867313+art3mes@users.noreply.github.com>
2023-05-25 14:11:49 +0530

    level 4: hashing + salting using bcrypt

--- Section 37- Authentication/Secrets/app.js
+++ Section 37- Authentication/Secrets/app.js
@@ -3,8 +3,10 @@ const express= require("express");
 const bodyParser= require("body-parser");
 const ejs=require("ejs");
 const mongoose = require("mongoose");
-const md5=require("md5");
+const md5=require("md5");                    //level 3 hashing
 //const encrypt=require("mongoose-encryption");           not required further than level 2 security
+const bcrypt = require('bcrypt');             //level 4 hashing + salting
+const saltRounds = 10;                    //more the salt rounds, more secure is the resultant hash at the expense of processing power. salt=31 can take 31 days on a 2ghz processor to make its hash
 const app=express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -36,31 +38,46 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 app.post("/register", function(req,res){
-    const newUser=new User({
-        email:req.body.username,
-        //password:req.body.password
-        password:md5(req.body.password)                //turning the entered password into a hash table to be stored in the db
+
+    const userpass = /^\$2y\$/.test(req.body.password) ? '$2a$' + req.body.password.slice(4) : req.body.password;   //https://github.com/kelektiv/node.bcrypt.js/issues/849#issuecomment-882636932
+    bcrypt.hash(userpass, saltRounds, function(err, hash) {
+        const newUser=new User({
+            email:req.body.username,
+            //password:req.body.password
+            //password:md5(req.body.password)                //turning the entered password into a hash table to be stored in the db level 3
+            password: hash
+        });
+        newUser.save()
+          .then(() => {
+            //res.send("User has been added successfully.");
+            res.render("secrets");
+          })
+          .catch(err => {
+            res.send(err);
+          });
     });
-    newUser.save()
-      .then(() => {
-        //res.send("User has been added successfully.");
-        res.render("secrets");
-      })
-      .catch(err => {
-        res.send(err);
-      });
+
+    
 });
 app.post("/login",function(req,res){
     const username=req.body.username;
-   // const password=req.body.password; for level 1 and 2
-    const password=md5(req.body.password);           //converting the entered password into a hash table for comparision with the stored password
+    const password=req.body.password;                   //for level 1 and 2 and 4
+    //const password=md5(req.body.password);           //converting the entered password into a hash table for comparision with the stored password level3
+    
 
     User.findOne({email:username})
     .then((foundUser)=>{
-
-        if(foundUser.password===password){
-            res.render("secrets");
-        } 
+        // if(foundUser.password===password){           using bcrypt for comparision instead
+        //     res.render("secrets");
+        // } 
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result===true){
+                res.render("secrets");
+            }
+            else{
+                res.send("fuck yourself");
+            }
+        });
     })
     .catch((err)=>{
         //console.log("An error occurred while finding the article:", err);
