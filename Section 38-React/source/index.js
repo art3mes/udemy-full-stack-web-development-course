458f9ff3669a3a1d6d1a562c2790f3835da35fab
art3mes <93867313+art3mes@users.noreply.github.com>
2023-06-02 21:31:13 +0530

    React

---
+++ Section 38-React/source/index.js
@@ -0,0 +1,8 @@
+//var React=require("react");
+//var ReactDOM=require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
+
+//ReactDOM.render(what to enter,where to enter,when to enter);
+//first argumment takes only one html element
+ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
