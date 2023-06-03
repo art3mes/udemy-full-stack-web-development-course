61c17ff1929a0be538ff5b965b769eb76c1f92bf
art3mes <93867313+art3mes@users.noreply.github.com>
2023-06-03 21:25:07 +0530

    370

--- Section 38-React/source/index.js
+++ Section 38-React/source/index.js
@@ -5,4 +5,16 @@ import ReactDOM from "react-dom";
 
 //ReactDOM.render(what to enter,where to enter,when to enter);
 //first argumment takes only one html element
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
+ReactDOM.render( document.getElementById("root"));
+ReactDOM.render(
+    <div>
+        <h1>Hello</h1>,
+      <h2>My Favourite Movies:</h2>
+      <ul>
+        <li>Thor: Ragnarok</li>
+        <li>DBS: Broly</li>
+        <li>Your Name</li>
+        </ul>
+      </div>
+      ,document.getElementById("root"));
+    
