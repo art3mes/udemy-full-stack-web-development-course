305aacd2c7278054359d1afbcc0c9c450585b380
art3mes <93867313+art3mes@users.noreply.github.com>
2023-06-03 21:54:00 +0530

    371

--- Section 38-React/source/index.js
+++ Section 38-React/source/index.js
@@ -11,10 +11,18 @@ ReactDOM.render(
         <h1>Hello</h1>,
       <h2>My Favourite Movies:</h2>
       <ul>
-        <li>Thor: Ragnarok</li>
-        <li>DBS: Broly</li>
-        <li>Your Name</li>
-        </ul>
+      <li>Thor: Ragnarok</li>
+    <li>DBS: Broly</li>
+    <li>Your Name</li>
+    </ul>
+    <h4>Hello {name1 +" "+name2}  way 1</h4>           ***********{} can run only js expressions. not statements******
+    <h2>Hello {name1} {name2} way 2</h2>
+    <h3>Hello {`${name1} ${name2}`}</h3> above line is interpreted as a string using the ES6 template literals. and then we've got that string being inserted in as JavaScript in these curly braces from JSX. And
+
+then it's being inserted as a string into our h1 and then that's being inserted as JavaScript code
+
+into a JavaScript file.
+    <p>Your lucky number is {Math.floor(Math.random()*10)}</p>
       </div>
       ,document.getElementById("root"));
     
