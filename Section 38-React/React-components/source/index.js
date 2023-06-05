e23d8dbe4a3ec64edcbad0626217d8c72a74db74
art3mes <93867313+art3mes@users.noreply.github.com>
2023-06-05 15:21:54 +0530

    376: React Components

--- Section 38-React/React-components/source/index.js
+++ Section 38-React/React-components/source/index.js
@@ -1,12 +1,17 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Heading from "./Heading"; //imported the heading component
+// import List from "./List"; //imported the list component
+
+// ReactDOM.render(
+//   <div>
+//     <Heading />
+//     <List />
+//   </div>,
+//   document.getElementById("root")
+// );
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading"; //imported the heading component
-import List from "./List"; //imported the list component
+import App from "./Components/App";
 
-ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
