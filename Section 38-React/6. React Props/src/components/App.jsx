67351a3623c5ccd9b9e0ce30b62d9d2a4d80614b
art3mes <93867313+art3mes@users.noreply.github.com>
2023-06-11 09:34:23 +0530

    388

--- Section 38-React/6. React Props/src/components/App.jsx
+++ Section 38-React/6. React Props/src/components/App.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id} //copy of key to which we can use
+      key={contact.id} //unique property. we cannot use it. used by react to get info about the elements
+      name={contact.name}
+      email={contact.email}
+      tel={contact.phone}
+      img={contact.imgURL}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
+      {contacts.map(createCard)}; {/* map function loops through all the elements of the contact element and calls the function */}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -23,7 +37,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
