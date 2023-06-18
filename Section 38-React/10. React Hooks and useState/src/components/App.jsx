f6aeb1508e5f27eb42f79b46d9b320132b724e82
art3mes <93867313+art3mes@users.noreply.github.com>
2023-06-18 19:51:56 +0530

    396: React hooks: useState()

---
+++ Section 38-React/10. React Hooks and useState/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+
+function App(){
+
+  const [count, setCount]=useState(0);
+
+  function inc(){
+    setCount(count + 1);
+  }
+  function dec(){
+    count===0?setCount(0):setCount(count -1);
+  }
+
+  return <div>
+    <h1>{count}</h1>
+    <button onClick={inc}>+</button>
+    <button onClick={dec}>-</button>
+  </div>;
+}
+export default App;
