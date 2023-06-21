7801239d26c82542ea7f3aee24a8ee5b0927fa2a
art3mes <93867313+art3mes@users.noreply.github.com>
2023-06-21 23:26:23 +0530

    forms

--- Section 38-React/12. Event handling/src/components/App.jsx
+++ Section 38-React/12. Event handling/src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 function App() {
   const [mousedOver, setMousedOver] = useState(false);
+  const [name, setName] = useState("");
+  const [heading, setHeading] = useState("");
 
   function toBlack() {
     setMousedOver(true);
@@ -10,14 +12,36 @@ function App() {
     setMousedOver(false);
   }
 
+  function handleChange(event) {
+    // console.log(event.target);
+    //  the event is the event which triggered the onChange
+
+    // console.log(event.target.placeholder);
+    // the target is the element that triggered this event and 
+    
+    // console.log(event.target.type);
+    // the value or any of the other properties  on the target are the things that correspond to the value of some of these attributes.
+    
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+  function changeHeading() {
+    setHeading(name);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {heading}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
       <button
         style={{ backgroundColor: mousedOver ? "black" : "white" }}
         onMouseOver={toBlack}
         onMouseOut={defaultColor}
+        onClick={changeHeading}
       >
         Submit
       </button>
@@ -26,3 +50,5 @@ function App() {
 }
 
 export default App;
+
+
