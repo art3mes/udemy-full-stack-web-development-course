d5221c2ed6e09284717a2992e5e2f2ac36247993
art3mes <93867313+art3mes@users.noreply.github.com>
2023-06-26 22:22:52 +0530

    403

--- Section 38-React/14. Complex States/src/components/App.jsx
+++ Section 38-React/14. Complex States/src/components/App.jsx
@@ -3,9 +3,10 @@ import React, { useState } from "react";
 function App() {
   // const [Fname, setFname] = useState("");
   // const [Lname, setLname] = useState("");
-  const [FullName, setFullName] = useState({
+  const [contact, setContact] = useState({
     fName: "",
-    lName: ""
+    lName: "",
+    email: ""
   });
   // function handleFChange(event) {
   //   setFname(event.target.value);
@@ -13,20 +14,46 @@ function App() {
   // function handleLChange(event) {
   //   setLname(event.target.value);
   // }
-  function changeFullName(event) {
+  // function changeFullName(event) {
+  //   const nameOfInput = event.target.name;
+  //   const newValue = event.target.value;
+
+  //   setFullName((prevValue) => {
+  //     if (nameOfInput === "fName") {
+  //       return {
+  //         fName: newValue,
+  //         lName: prevValue.lName
+  //       };
+  //     } else if (nameOfInput === "lName") {
+  //       return {
+  //         fName: prevValue.fName,
+  //         lName: newValue
+  //       };
+  //     }
+  //   });
+  // }
+  
+  function handleChange(event) {
     const nameOfInput = event.target.name;
     const newValue = event.target.value;
-
-    setFullName((prevValue) => {
+    setContact((prevValue) => {
       if (nameOfInput === "fName") {
         return {
           fName: newValue,
-          lName: prevValue.lName
+          lName: prevValue.lName,
+          email: prevValue.email
         };
       } else if (nameOfInput === "lName") {
         return {
           fName: prevValue.fName,
-          lName: newValue
+          lName: newValue,
+          email: prevValue.email
+        };
+      } else if (nameOfInput === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: newValue
         };
       }
     });
@@ -34,25 +61,33 @@ function App() {
   return (
     <div className="container">
       <h1>
-        Hello {FullName.fName} {FullName.lName}
+        Hello {contact.fName} {contact.lName}
       </h1>
+      <p>{contact.email}</p>
       <form>
         <input
-          onChange={changeFullName}
+          onChange={handleChange}
           name="fName"
           placeholder="First Name"
-          value={FullName.fName}
+          value={contact.fName}
         />
         <input
-          onChange={changeFullName}
+          onChange={handleChange}
           name="lName"
           placeholder="Last Name"
-          value={FullName.lName}
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
         />
         <button>Submit</button>
       </form>
     </div>
   );
 }
+
 
 export default App;
