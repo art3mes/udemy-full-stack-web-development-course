e62cbf2878df2bc5948771d32c6f6e7ba0bc934e
art3mes <93867313+art3mes@users.noreply.github.com>
2023-07-01 12:45:36 +0530

    bug 1: clear add area after addition. bug 2: delete button color

--- Section 38-React/16. Keeper app/src/components/CreateArea.jsx
+++ Section 38-React/16. Keeper app/src/components/CreateArea.jsx
@@ -18,6 +18,10 @@ function CreateArea(props) {
   }
   function addNote(event) {
     props.onAdd(note);
+    setNote({
+      title:"",
+      content:""
+    });
     event.preventDefault();
   }
   return (
