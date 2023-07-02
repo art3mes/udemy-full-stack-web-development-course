d611fb1285710373c4881fe4a253d6b9ff0eaad2
art3mes <93867313+art3mes@users.noreply.github.com>
2023-07-02 22:22:01 +0530

    zoom

--- Section 38-React/16. Keeper app/src/components/CreateArea.jsx
+++ Section 38-React/16. Keeper app/src/components/CreateArea.jsx
@@ -1,52 +1,47 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import Fab from '@mui/material/Fab';
-
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
-  const [note, setNote] = useState({
-    title: "",
-    content: ""
-  });
+    const [note,
+        setNote] = useState({title: "", content: ""});
 
-  function handleChange(event) {
-    const { name, value } = event.target;
+    function handleChange(event) {
+        const {name, value} = event.target;
 
-    setNote((preValue) => {
-      return {
-        ...preValue,
-        [name]: value
-      };
-    });
-  }
-  function addNote(event) {
-    props.onAdd(note);
-    setNote({
-      title:"",
-      content:""
-    });
-    event.preventDefault();
-  }
-  return (
-    <div>
-      <form className="create-note">
-        <input
-          onChange={handleChange}
-          value={note.title}
-          name="title"
-          placeholder="Title"
-        />
-        <textarea
-          onChange={handleChange}
-          value={note.content}
-          name="content"
-          placeholder="Take a note..."
-          rows="3"
-        />
-        <Fab onClick={addNote}><AddCircleOutlineIcon /></Fab>
-      </form>
-    </div>
-  );
+        setNote((preValue) => {
+            return {
+                ...preValue,
+                [name]: value
+            };
+        });
+    }
+    function addNote(event) {
+        props.onAdd(note);
+        setNote({title: "", content: ""});
+        event.preventDefault();
+    }
+    return (
+        <div>
+            <form className="create-note">
+                <input
+                    onChange={handleChange}
+                    value={note.title}
+                    name="title"
+                    placeholder="Title"/>
+                <textarea
+                    onChange={handleChange}
+                    value={note.content}
+                    name="content"
+                    placeholder="Take a note..."
+                    rows="3"/>
+                <Zoom in={true}>
+                    <Fab onClick={addNote}><AddCircleOutlineIcon/></Fab>
+                </Zoom>
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
